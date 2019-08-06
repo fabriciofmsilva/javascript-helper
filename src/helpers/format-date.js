a17b0a7d95996fa1625284ac8dac4ddeb3ee4096
Fabrício Silva <fabriciofmsilva@gmail.com>
2019-08-06 15:01:41 -0300

    Add format date

---
+++ src/helpers/format-date.js
@@ -0,0 +1,16 @@
+/*!
+ * Convert a timestamp into a date
+ * (c) 2019 Chris Ferdinandi, MIT License, https://gomakethings.com
+ * @param   {String|Integer}  timestamp  The timestamp in unix of YYYY-MM-DD HH:MM:SS format
+ * @returns {String}                     A formatted date
+ */
+var formatDate = function (timestamp) {
+	// Create a date object from the timestamp
+	var date = new Date(timestamp);
+
+	// Create a list of names for the months
+	var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',	'November', 'December'];
+
+	// return a formatted date
+	return months[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear();
+};
